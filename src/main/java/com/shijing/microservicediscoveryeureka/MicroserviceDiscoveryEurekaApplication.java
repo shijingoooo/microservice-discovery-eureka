1ce25d6c37a7aab9bc1fc1f9efb4a5580c27246b
shijingoooo <shijingoooo@gmail.com>
2019-08-24 23:12:24 +0800

    first commit

---
+++ src/main/java/com/shijing/microservicediscoveryeureka/MicroserviceDiscoveryEurekaApplication.java
@@ -0,0 +1,33 @@
+package com.shijing.microservicediscoveryeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MicroserviceDiscoveryEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceDiscoveryEurekaApplication.class, args);
+    }
+
+    @EnableWebSecurity
+    static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().ignoringAntMatchers("/eureka/**");
+//            http.csrf().disable()
+//                    .authorizeRequests()
+//                    .anyRequest().authenticated()
+//                    .and()
+//                    .httpBasic();
+            super.configure(http);
+        }
+    }
+
+}
---
+++ src/main/java/com/shijing/microservicediscoveryeureka/config/WebSecurityConfig.java
@@ -0,0 +1,17 @@
+package com.shijing.microservicediscoveryeureka.config;
+
+
+/**
+ * 关闭csrf检验
+ * @author shijing
+ *
+ */
+//@EnableWebSecurity
+//class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.csrf().ignoringAntMatchers("/eureka/**");
+//        super.configure(http);
+//    }
+//}
